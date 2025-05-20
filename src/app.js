682ca2d53633fc23efcb956af46ef4a0c7fa9aa0
destiny <theimpactindex@gmail.com>
2025-05-20 19:04:45 +0100

    welcome

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import rootRoutes from './routes/rootRoutes.js';
-import dbConnection from '../config/dbConnection.js';
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import rootRoutes from "./routes/rootRoutes.js";
+import dbConnection from "../config/dbConnection.js";
 
-
-const PORT =  process.env.SERVER_PORT || 3500;
+const PORT = process.env.SERVER_PORT || 3500;
 
 const app = express();
 app.use(express.json());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
+app.use(
+  cors({
     methods: ["*"],
-    origin: ["http://localhost:3000", "http:localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http:localhost:5173",
+      "https://toshen-backend.onrender.com",
+      "https://toshel-frontend.vercel.app/",
+    ],
     optionsSuccessStatus: 200,
-}))
+  })
+);
 
 app.use(helmet());
 
@@ -28,11 +34,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 rootRoutes(app);
 
-app.use((req, res, next) => res.status(404).json({message: "Endpoint not found!"}))
+app.use((req, res, next) =>
+  res.status(404).json({ message: "Endpoint not found!" })
+);
 
 app.listen(PORT, async () => {
-    
-    await dbConnection();
+  await dbConnection();
 
-    console.log(`Server is running at port ${PORT}`);
-})
+  console.log(`Server is running at port ${PORT}`);
+});
